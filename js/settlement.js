1cb77ed99b1e375b34ad43851e5df8fd2013e988
chirpmonster <598033121@qq.com>
2020-03-30 21:47:42 +0800

    test

---
+++ js/settlement.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("img.leaveMessageImg").click(function(){
+        $(this).hide();
+        $("span.leaveMessageTextareaSpan").show();
+        $("div.orderItemSumDiv").css("height","100px");
+    });
+});
